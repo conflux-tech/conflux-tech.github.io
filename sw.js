b8a462cf8c1edee88512c8167530dd31b11dc0d3
Junerey Casuga <junerey.casuga@gmail.com>
2020-06-26 16:01:08 +0700

    Updates

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-4abceb9289b82504f688.js"
+    "url": "webpack-runtime-5c02982eb00106455a5e.js"
   },
   {
     "url": "framework-868c53fff4771b0bde98.js"
@@ -37,7 +37,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "407e35cd6a80918da11a009ce319145e"
+    "revision": "f4829467f22f130c9fc807b7ee1758fb"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-7c31e2436cade51cbcda.js"
